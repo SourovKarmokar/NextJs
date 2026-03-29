6c5ac4744dbf4e69889dca9a2caad7a9921a6877
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-03-29 21:47:58 +0600

    doc & layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { getDocuments } from "@/lib/doc";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,12 +18,17 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  getDocuments();
+
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
