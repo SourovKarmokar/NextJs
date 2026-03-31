3b98706c0cc1611f7722f88a2875cd8bea0e1b7e
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-03-31 19:46:28 +0600

    grobal css & layout and Header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 import { getDocuments } from "@/lib/doc";
 
 const geistSans = Geist({
@@ -13,14 +14,13 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "DocuCraft - A documentation website by Protocal ",
+  title: "DocuCraft - A documentation website by Protocal",
   description: "A documentation website by Protocal",
 };
 
 export default function RootLayout({ children }) {
-  const allDocuments =  getDocuments();
+  const allDocuments = getDocuments();
   console.log(allDocuments);
-  
 
   return (
     <html
@@ -29,7 +29,23 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
-        {children}
+        <Header  docs={allDocuments} />
+        <div className="relative px-4 pt-14 sm:px-6 lg:px-8"> 
+          
+          
+          <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
+            <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
+              <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100" />
+            </div>
+          </div>
+
+          <main className="flex-auto py-16"> 
+            <article className="prose dark:prose-invert"> 
+              {children}
+            </article> 
+          </main> 
+
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-
 export default function Home() {
   return (
     <div>Landig Page</div>
